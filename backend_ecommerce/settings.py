bf5e76b02b1528f7987fef5d841e85697e140145
sathish3201 <sathishchakali1023@gmail.com>
2025-03-01 19:52:42 +0530

    allowed host commit

--- backend_ecommerce/settings.py
+++ backend_ecommerce/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', os.getenv('DEBUG')) 
 
-ALLOWED_HOSTS = []
-# [os.environ.get("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
 
 
 # Application definition
