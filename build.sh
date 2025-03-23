8e99880eb5a5c26ebfab6522f7207d80b63a4484
sathish3201 <sathishchakali1023@gmail.com>
2025-03-23 09:40:17 +0530

    adding superuser

--- build.sh
+++ build.sh
@@ -4,6 +4,24 @@
 set -o errexit
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+
 python manage.py migrate
 
+
+python manage.py shell <<EOF 
+# check if super already exists
+import os
+from django.contrib.auth import get_user_model 
+
+# get the envirolment variables
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", os.getenv("DJANGO_SUPERUSER_USERNAME"))
+email = os.environ.get("DJANGO_SUPERUSER_USERNAME", os.getenv("DJANGO_SUPERUSER_EMAIL"))
+password =os.environ.get("DJANGO_SUPERUSER_USERNAME", os.getenv("DJANGO_SUPERUSER_PASSWORD"))
+
+User = get_user_model()
+#Check if super user exist or not
+if not User.objects.filter(username = username).exists():
+    User.objects.create_superuser(username= username, email=email, password=password)
+EOF
+
+python manage.py collectstatic --no-input
